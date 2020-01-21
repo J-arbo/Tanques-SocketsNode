4d01107e26413b4a5903de9d9fb54338ab3b8e21
Martypose <46140279+Martypose@users.noreply.github.com>
2020-01-21 20:31:34 +0100

    Create main.js

---
+++ server/main.js
@@ -0,0 +1,34 @@
+
+
+function jugador() {
+    this.color;
+    this.puntuacion=0;
+    this.tieneTanque=false;
+
+    function nuevoTanque () {
+
+
+    }
+
+
+
+};
+
+
+function tanque() {
+
+    this.positionX;
+    this.positionY;
+    this.retraso;
+    this.vidas=2;
+
+
+    function dispara() {
+        
+
+    }
+
+
+
+};
+
